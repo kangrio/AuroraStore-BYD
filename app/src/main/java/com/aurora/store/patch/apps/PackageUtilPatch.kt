cc8aceef8a56de172e2cb4e384ef859fca748856
KangRio <masolae.sa@gmail.com>
2026-08-13 03:12:11 +0700

    fix: improve canRequestPackageInstalls check to support Root, Shizuku, and AM installers

--- app/src/main/java/com/aurora/store/patch/apps/PackageUtilPatch.kt
+++ app/src/main/java/com/aurora/store/patch/apps/PackageUtilPatch.kt
@@ -3,9 +3,21 @@ package com.aurora.store.patch.apps
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.content.pm.PackageInfoCompat
+import com.aurora.store.data.installer.AppInstaller
+import com.aurora.store.data.model.Installer
 import com.aurora.store.util.PackageUtil
 
 object PackageUtilPatch {
+    fun canRequestPackageInstalls(context: Context): Boolean {
+        val currentInstaller = AppInstaller.getCurrentInstaller(context)
+        return when (currentInstaller) {
+            Installer.ROOT -> AppInstaller.hasRootAccess()
+            Installer.AM -> AppInstaller.hasAppManager(context)
+            Installer.SHIZUKU -> AppInstaller.hasShizukuOrSui(context) && AppInstaller.hasShizukuPerm()
+            else -> false
+        }
+    }
+
     fun isUpdatable(context: Context, packageName: String, versionCode: Long): Boolean {
         return try {
             // original apps
